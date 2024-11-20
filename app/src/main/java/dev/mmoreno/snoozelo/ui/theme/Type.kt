e9a290e7bc52f4995ccfd62b1923abf19ecdcb98
Mario Moreno <mariomoreno.cert@gmail.com>
2024-11-20 11:33:32 -0600

    Added typography stiles to match design, use of Montserrat font family.

--- app/src/main/java/dev/mmoreno/snoozelo/ui/theme/Type.kt
+++ app/src/main/java/dev/mmoreno/snoozelo/ui/theme/Type.kt
@@ -6,11 +6,29 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
 val Typography = Typography(
+    titleLarge = TextStyle(
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 26.sp,
+        letterSpacing = 0.5.sp
+    ),
     bodyLarge = TextStyle(
         fontFamily = MontserratFontFamily,
         fontWeight = FontWeight.Normal,
         fontSize = 16.sp,
         lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 18.sp,
+        letterSpacing = 0.5.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = MontserratFontFamily,
+        fontWeight = FontWeight.Light,
+        fontSize = 12.sp,
         letterSpacing = 0.5.sp
     )
 )
